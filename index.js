b8b66a5309c37ed5be96bc9d0bf3266ac6ce9e2e
nicohnavarro <nicohnavarro@gmail.com>
2021-05-22 02:34:16 -0300

    :fire: full restapi skeleton

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config;
+
+const  express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.get('/', function (req,res){
+  res.send('Hello world')
+})
+
+app.listen(port)
